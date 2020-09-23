6baab0a9142d5910a962b92f0a48b1d07c8ac3e3
Tutu12344 <padde.cross@gmail.com>
2020-09-24 07:59:50 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
+// import React, { Component } from "react";
+// import { ImagePropTypes } from "react-native";
 import React from "react";
+// class App extends Component {
+//   render() {
+//     return(
+//     <div>
+//       <Cat name={"tTaro"}/>
+//     </div>)
+//   }
+// }
 
-function App() {
+const App = () => {
+  const profiles = [
+    {
+      name: "taro",
+      age: 10,
+    },
+    {
+      name: "t",
+      age: 0,
+    },
+  ];
   return (
     <div>
-      <h1>helloWorld</h1>
+      {profiles.map((profile, index) => {
+        return <Cat name={profile.name} age={profile.age} key={index} />;
+      })}
+      {/* <Cat name={"tTaro"} age={10} /> */}
     </div>
   );
-}
+};
+// function App() {
+//   return <div>helloWorld</div>;
+// }
 
+const Cat = (props) => {
+  return (
+    <div>
+      This {props.name}
+      {props.age}{" "}
+    </div>
+  );
+};
+//defaultのあたい
+Cat.defaultProps = {
+  age: 1,
+};
 export default App;
