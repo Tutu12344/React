b94557a5bed9030821bbe02928e67646c8d6658a
Tutu12344 <padde.cross@gmail.com>
2020-09-25 07:54:23 +0900

    delete events learn

---
+++ src/components/events_show.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { get_events, delete_events, put_events } from "../actions";
+import { Link } from "react-router-dom";
+import { Field, reduxForm } from "redux-form";
+
+class EventsShow extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onDeleteClick = this.onDeleteClick.bind(this);
+  }
+  renderField(field) {
+    const {
+      input,
+      label,
+      type,
+      meta: { touched, error },
+    } = field;
+    return (
+      <div>
+        {console.log(input)}
+        {/* ...可変長配列の展開 */}
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    );
+  }
+  async onSubmit(values) {
+    // await this.props.post_events(values);
+    this.props.history.push("/");
+  }
+  async onDeleteClick() {
+    // console.log(this.props.match);
+    const { id } = this.props.match.params;
+    // console.log(id);
+    await this.props.delete_events(id);
+    this.props.history.push("/");
+  }
+  render() {
+    const { handleSubmit, pristine, submitting } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <div>
+          <Field
+            label="Title"
+            name="title"
+            type="text"
+            component={this.renderField}
+          ></Field>
+          <Field
+            label="Body"
+            name="body"
+            type="text"
+            component={this.renderField}
+          ></Field>
+        </div>
+        <div>
+          <input
+            type="submit"
+            value="Submit"
+            disabled={pristine || submitting}
+          />
+          <Link to="/">Cancel</Link>
+          <Link to="/" onClick={this.onDeleteClick}>
+            Delete
+          </Link>
+        </div>
+      </form>
+    );
+  }
+}
+const validate = (values) => {
+  const errors = {};
+  if (!values.title) errors.title = "Enter a title,please";
+  if (!values.body) errors.body = "Enter a body,please";
+  return errors;
+};
+const mapDispatchToProps = { delete_events };
+export default connect(
+  null,
+  mapDispatchToProps
+)(reduxForm({ validate, form: "eventShowForm" })(EventsShow));
